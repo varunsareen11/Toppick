17cedc3889cb97744fbeddf904ba4e04731fd02f
varunsareen11 <varunsareen11@gmail.com>
2022-01-31 20:25:58 +0530

    Update-31-01-2022

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,7 @@
-// Loader design
 window.onload = function () {
   setTimeout(function () {
     var loader = document.getElementsByClassName("loader")[0];
     loader.className = "loader fadeout";
-    setTimeout(function () {
-      loader.style.display = "none";
-    }, 1000);
   }, 1000);
 };
 
